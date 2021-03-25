2f45e795e64849b6383e29af74252ef32f325458
IuriiKyrylovskyi <iurii712@ukr.net>
2021-03-25 20:12:14 +0200

    1.2

--- lesson_1/1.2/src/scripts/index.js
+++ lesson_1/1.2/src/scripts/index.js
@@ -1,7 +1,7 @@
-import { initTodoListHandlers } from "../todoList/todoList.js";
-import { renderTasks } from "../renderTasks/renderTasks.js";
-import { getTasksList } from "../tasksGateway/tasksGateway.js";
-import { setItem } from "../storage/storage.js";
+import { initTodoListHandlers } from "./todoList/todoList.js";
+import { renderTasks } from "./renderTasks/renderTasks.js";
+import { getTasksList } from "./tasksGateway/tasksGateway.js";
+import { setItem } from "./storage/storage.js";
 
 document.addEventListener('DOMContentLoaded', () => {
   getTasksList().then(taskList => {
