d13a2f32fdc8fd97eb8c5806d9a17677070d3387
IuriiKyrylovskyi <iurii712@ukr.net>
2021-03-25 23:26:16 +0200

    testing_undone

--- lesson_1/1.2/src/scripts/createTask/__test__/createTask.spec.js
+++ lesson_1/1.2/src/scripts/createTask/__test__/createTask.spec.js
@@ -1,5 +1,9 @@
 import { onCreateTask } from "../createTask.js";
 
-it('should return new task object with empty', () => {
-	const text = 
-})
+
+it("should return new task object with text: \"to buy milk\"", () => {
+	const text = "to buy milk";
+	const newTask = onCreateTask();
+	
+	expect(newTask).toEqual({ text: "to buy milk", done: false, createDate: new Date().toISOString() }); 
+});
