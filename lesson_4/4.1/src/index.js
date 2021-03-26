88dbe0fcc1c54cbc0b227bcaa2ba2b70548049ea
IuriiKyrylovskyi <iurii712@ukr.net>
2021-03-26 17:54:29 +0200

    4.1

--- lesson_4/4.1/src/index.js
+++ lesson_4/4.1/src/index.js
@@ -5,6 +5,11 @@
 // импортируйте ф-цию fetchUser из profile/gateway.js под таким же именем
 // импортируйте ф-цию printProfile из profile/index.js под таким же именем
 
+// impor {} from './polyfills.js'
+import calcExpression, { sum, mult } from "./calculator/index.js";
+import  fetchUser  from "./profile/gateway.js";
+import { printProfile } from "./profile/index.js";
+
 const calcResult = calcExpression("1 + 2");
 const sumResult = sum(1, 2);
 const multResult = mult(1, 2);
@@ -12,3 +17,8 @@ const userDataPromise = fetchUser("facebook");
 printProfile({ name: "Tom", from: "The World" });
 
 // убедитесь, скрипт выполнился без ошибок
+console.log(calcResult);
+console.log(sumResult);
+console.log(multResult);
+console.log(userDataPromise);
+console.log(printProfile({ name: "Tom", from: "The World" }));
