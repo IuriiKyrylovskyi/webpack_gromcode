62433ddef9108b65576a7b14fd1de11c5adde837
IuriiKyrylovskyi <iurii712@ukr.net>
2021-03-26 20:17:03 +0200

    5.1

--- lesson_5/5.1/src/index.js
+++ lesson_5/5.1/src/index.js
@@ -3,7 +3,7 @@
 import fetchUser from "./profile/gateway.js";
 import { printProfile } from "./profile/index.js";
 
-fetchUser("git").then((userData) =>
+fetchUser("fifa").then((userData) =>
   printProfile({
     name: userData.name,
     company: userData.location,
