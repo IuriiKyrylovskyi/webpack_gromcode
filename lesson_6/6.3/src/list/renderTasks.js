9dfc7e85101f5b42afe0b3fa9823ddcd27882107
IuriiKyrylovskyi <iurii712@ukr.net>
2021-03-27 19:23:06 +0200

    6.3

---
+++ lesson_6/6.3/src/list/renderTasks.js
@@ -0,0 +1,57 @@
+import { getItem } from "./storage.js";
+import "./list.scss";
+
+const listElem = document.querySelector(".list");
+
+const compareTasks = (a, b) => {
+  if (a.done - b.done !== 0) {
+    return a.done - b.done;
+  }
+
+  if (a.done) {
+    return new Date(b.finishDate) - new Date(a.finishDate);
+  }
+
+  return new Date(b.createDate) - new Date(a.createDate);
+};
+
+const createCheckbox = ({ done, id }) => {
+  const checkboxElem = document.createElement("input");
+  checkboxElem.setAttribute("data-id", id);
+  checkboxElem.setAttribute("type", "checkbox");
+  checkboxElem.checked = done;
+  checkboxElem.classList.add("list__item-checkbox");
+
+  return checkboxElem;
+};
+
+const createListItem = ({ text, done, id }) => {
+  const listItemElem = document.createElement("li");
+  listItemElem.classList.add("list__item");
+
+  const checkboxElem = createCheckbox({ done, id });
+  if (done) {
+    listItemElem.classList.add("list__item_done");
+  }
+
+  const textElem = document.createElement("span");
+  textElem.classList.add("list__item-text");
+  textElem.textContent = text;
+
+  const deleteBtnElem = document.createElement("button");
+  deleteBtnElem.classList.add("list__item-delete-btn");
+  deleteBtnElem.dataset.id = id;
+
+  listItemElem.append(checkboxElem, textElem, deleteBtnElem);
+
+  return listItemElem;
+};
+
+export const renderTasks = () => {
+  const tasksList = getItem("tasksList") || [];
+
+  listElem.innerHTML = "";
+  const tasksElems = tasksList.sort(compareTasks).map(createListItem);
+
+  listElem.append(...tasksElems);
+};
