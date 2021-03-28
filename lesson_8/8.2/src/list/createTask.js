f0c5851b3e6be8728d3fee950b9a65e132e3031c
IuriiKyrylovskyi <iurii712@ukr.net>
2021-03-28 22:41:30 +0300

    8.2

---
+++ lesson_8/8.2/src/list/createTask.js
@@ -0,0 +1,36 @@
+import { createTask, getTasksList } from "./tasksGateway.js";
+import { renderTasks } from "./renderTasks.js";
+import { setItem } from "./storage.js";
+
+export const onCreateTask = () => {
+  const taskTitleInputElem = document.querySelector('.task-input');
+
+  const text = taskTitleInputElem.value;
+
+  if (!text) {
+    return;
+  }
+  taskTitleInputElem.value = '';
+
+  const newTask = {
+    text,
+    done: false,
+    createDate: new Date().toISOString(),
+    // id: Math.random().toString(), // server generates id
+  };
+
+  createTask(newTask)
+    .then(() => getTasksList())
+    .then(newTasksList => {
+      setItem('tasksList', newTasksList);
+      renderTasks();
+    });
+
+
+};
+
+// 1. Prepare data
+// 2. Write data to db
+// 3. Read new data from servre
+// 4. Save new data to front - end storage
+// 5. Update UI based on new data
